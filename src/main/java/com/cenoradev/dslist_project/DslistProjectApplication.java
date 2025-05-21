5ef281eaa243ec8f4bb8e2c7ee1a9604c7ff904e
Itallo Eduardo <edu123ww@gmail.com>
2025-05-21 17:45:24 -0300

    first upload

---
+++ src/main/java/com/cenoradev/dslist_project/DslistProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cenoradev.dslist_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cenoradev/dslist_project/DslistProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cenoradev.dslist_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
